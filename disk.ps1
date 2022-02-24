cb470da4608b8c1fa60306286249a8d76dc0351f
Nathan <nathan.bourgoin01@gmail.com>
2022-02-24 10:30:20 -0500

    post verification

--- disk.ps1
+++ disk.ps1
@@ -25,10 +25,42 @@ $logFile = "$logPath\diskCheck.log"
 
 #Vérifier si le répertoire est existant
 try {
+    #Répertoire n'est pas trouvé
     if(-not (Test-Path -Path $logPath -ErrorAction Stop)){
-
+        New-Item -ItemType Directory -Path $logPath -ErrorAction Stop | Out-Null  #Out-Null permet de ne pas afficher plein de lignes dans la console
+        New-Item -ItemType File -Path $logFile | Out-Null
     }
 }
+#Attraper les erreurs pour ne pas faire crash
 catch {
+    throw
+}
+
+#Commentaire de début de script pour dire d'ou la commande a été exécuté dans les logs
+Add-Content -Path $logFile -Value "[INFO] Execution en cours de $PSCommandPath"
+
+#Vérifier que PoshGram est installé sinon on log l'erreur dans notre fichier log
+if(-not (Get-Module -Name PoshGram -ListAvailable)){
+    Add-Content -Path $logFile -Value "[ERREUR] PoshGram n'est pas installé."
+    throw
+}
+else{
+    Add-Content -Path $logFile -Value "[INFO] PoshGram est installé."
+}
+
+#Collecter les informations du disque dur
+
+if($PSVersionTable.Platform -eq 'Unix'){ #les commandes sont différentes sur linux donc on doit avoir deux sections de commandes selon le OS
+    #used
+    #free
+    $volume = Get-PSDrive -Name $Drive
+    #Vérifier si le drive existe
+    if($volume){
+        $total = $volume.Used + $volume.Free
+    }else{
+        Add-Content -Path $logFile -Value "[ERREUR] $Drive n'est pas existant"
+    }
+}
+else{
     
 }
