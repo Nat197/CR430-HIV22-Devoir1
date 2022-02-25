99e4776f099422b305bc4193887659996250acb7
Gabriel Bellemare <gabrielbellemare13@gmail.com>
2022-02-24 20:42:23 -0500

    Push bonne version performance, dois etre formater et transformer en tache.

--- performancePS.ps1
+++ performancePS.ps1
@@ -1,38 +1,63 @@
-
-$computerName = $env:COMPUTERNAME
-
-$cpu = Get-Counter -Counter "\Processeur(_Total)\% temps processeur" -SampleInterval 1 -MaxSamples 1
-$memoryFreeSpaceMegabytes = Get-Counter -Counter "\Mémoire\Mégaoctets disponibles" -SampleInterval 1 -MaxSamples 1 
-$networkBandePassanteWIFI = Get-Counter -Counter "\Interface réseau(Intel[R] Wi-Fi 6 AX201 160MHz)\Bande passante actuelle" -SampleInterval 1 -MaxSamples 1 
-$physicalDiskTotalOctets = Get-Counter -Counter "\Disque physique(_Total)\Octets disque/s" -SampleInterval 1 -MaxSamples 1 
-$physicalDiskPourcentageInactivite = Get-Counter -Counter "\Disque physique(_Total)\% d’inactivité" -SampleInterval 1 -MaxSamples 1 
-        
-$properties = @{
-    cpu = $cpu.CounterSamples.CookedValue
-    memoryFreeSpaceMegabytes = $memoryFreeSpaceMegabytes.CounterSamples.CookedValue
-    networkBandePassanteWIFI = $networkBandePassanteWIFI.CounterSamples.CookedValue
-    physicalDiskTotalOctets = $physicalDiskTotalOctets.CounterSamples.CookedValue
-    physicalDiskPourcentageInactivite = $physicalDiskPourcentageInactivite.CounterSamples.CookedValue
-}
-$exportObject = New-Object psobject -Property $properties
 #Repertoire ou le fichier de resultat sera sauvergarder
-$RepertoireSortie = "C:\DevoirCR430_Performance-Counters"
+$RepertoireSortie = "C:\Compteurs-des-Performances"
+
+#Set le nom de l'ordinateur pour lequel les donnees de performance seront collecter.
+#Laissez vide pour l'ordinateur local
+$NomOrdinateur = ""
+
+#Collection d'interval par secondes
+$sampleInterval = 15
+
+#Nombre d'echantillon qu'on veut collecter. 
+#Set le nombre a 0 si on veut un collection continue.
+$maxSamples  = 240
 
 #Verifie si le repertoire de sortie existe, si non il va le creer.
 if (-not(Test-Path $RepertoireSortie))
-{
-    Write-Host "Le repertoire de sorti n'existe pas. Un repertoire va etre creer."
-    $null = New-Item -Path $RepertoireSortie -ItemType "Directory"
-    Write-Host "Un repertoire de sorti a ete creer"
-}
+    {
+        
+        Write-Host "Le repertoire de sorti n'existe pas. Un repertoire va etre creer."
+        $null = New-Item -Path $RepertoireSortie -ItemType "Directory"
+        Write-Host "Un repertoire de sorti a ete creer"
+    }
 
 #Enleve le \ a la fin du Path si nessecaire.
 if ($RepertoireSortie.EndsWith("\")) {$RepertoireSortie = $RepertoireSortie.Substring(0, $RepertoireSortie.Length - 1)}
 
-$FichierSortie = "$RepertoireSortie\{$computerName}) $(Get-Date -Format "yyyy_MM_dd HH_mm_ss")-Performance.csv" 
+#Creation du nom de fichier de sortie comme etant : le nom de l'ordinateur suivi de la date.csv
+$FichierSortie = "$RepertoireSortie\$(if($NomOrdinateur -eq '')
+                                        {$env:COMPUTERNAME} 
+                                        else 
+                                        {$NomOrdinateur}) $(Get-Date -Format "yyyy_MM_dd HH_mm_ss").csv"
+ 
+#Affiche les parametres a l'ecran.
+Write-Host "
+ 
+Recuperation des donnees...
+Entrez Ctrl+C pour quitter."
+ 
+#Specify the list of performance counters to collect.
+    $counters =(
+        "\Processeur(_Total)\% temps processeur",
+        "\Mémoire\Mégaoctets disponibles",
+        "\Interface réseau(Intel[R] Wi-Fi 6 AX201 160MHz)\Bande passante actuelle",
+        "\Disque physique(_Total)\Octets disque/s"
+    )
+
+    $variables = @{
+        SampleInterval = $sampleInterval
+        Counter = $counters
+    }
 
 
-Write-Host $exportObject
-Export-Csv -Path $FichierSortie -InputObject $exportObject -Force
+     
+#Either set the sample interval or specify to collect continuous.
+if ($maxSamples -eq 0 -or $null) {$variables.Add("Continue",1)}
+else {$variables.Add("MaxSamples","$maxSamples")}
+    
+#Ajout du nom de l'ordinateur si le nom n'est pas vide
+if ($NomOrdinateur -ne "" -or $null) {$variables.Add("ComputerName","$NomOrdinateur")}
 
-
+#Show the variables then execute the command while storing the results in a file.
+Get-Counter @Variables | Export-Csv -Path $FichierSortie -Force
+#------------------------------------------------------------------------------------
