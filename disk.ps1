2c38af2270abb33b41c2a0488097ea40240ef9d0
Nathan <nathan.bourgoin01@gmail.com>
2022-02-25 12:13:52 -0500

    Suppression commande slinux

--- disk.ps1
+++ disk.ps1
@@ -19,14 +19,8 @@ param (
 )
 
 #Emplacement des logs
+$logPath = 'C:\Logs'
 
-#Vérification du OS et sélection de l'emplacement selon le OS
-# if ($PSVersionTable.Platform -eq 'Unix') {
-#     $logPath = '/tmp'
-# }
-# else {
-    $logPath = 'C:\Logs'
-# }
 
 #Chemin complet vers le fichier log
 $logFile = "$logPath\diskCheck.log"
@@ -62,27 +56,6 @@ else {
 
 #Si une erreur survient le -ErrorAction Stop va attraper cette erreur
 try {
-    # #Systeme Linux
-    # if ($PSVersionTable.Platform -eq 'Unix') {
-    #     #les commandes sont différentes sur linux donc on doit avoir deux sections de commandes selon le OS
-    #     #used
-    #     #free
-    #     $volume = Get-PSDrive -Name $Drive -ErrorAction Stop
-    #     #Vérifier si le drive existe
-    #     if ($volume) {
-    #         $total = $volume.Used + $volume.Free  #déterminer l'espace total de notre disque
-    #         $espaceLibre = [int](($volume.Free / $total) * 100)  #Division espace libre avec espace total converti en pourcentage
-
-    #         Add-Content -Path $logFile -Value "[INFO] Espace libre : $espaceLibre%"
-    #     }
-    #     else {
-    #         Add-Content -Path $logFile -Value "[ERREUR] $Drive n'est pas existant"
-    #         throw
-    #     }
-    # }
-    #Systeme Windows
-    # else {
-        #Sélectionner le disque dont la lettre correspond a celle spécifié par l'utilisateur
         $volume = Get-Volume -ErrorAction Stop | Where-Object{$_.DriveLetter -eq $Drive} 
 
         if ($volume) {
